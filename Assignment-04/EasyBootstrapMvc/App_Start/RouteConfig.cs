b898297223884f3dcca3543f8e20117b1622658b
JUSTIN SPENCER <jspencer.jms@gmail.com>
2016-04-09 19:06:56 -0400

    Adding assn4 (web stuff)
    An MVC app that shows off some routing, making markup, layout files, and JQuery selectors

---
+++ Assignment-04/EasyBootstrapMvc/App_Start/RouteConfig.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace EasyBootstrapMvc
+{
+    /// <summary>
+    /// This is where you set up all of the routes that make a URL go to your app.
+    /// For example http://localhost:65018/Home/Index will go to HomeController, which then renders the "Index" View.
+    /// The "defaults" provide default values, so that http://localhost:65018/ will actually go to http://localhost:65018/Home/Index by default!
+    /// </summary>
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}"); // this is a special route that .NET generates when making bundles. Ignore it for now.
+
+            // TODO: Put your new route here (routes match in order, so depending on the route you create, it might never be reached if other routes match first!)
+
+            routes.MapRoute(
+                name: "Default",                                        // name of the route
+                url: "{controller}/{action}",                           // Values that go into the URL (for example, localhost/{Home}/{Index}
+                defaults: new { controller = "Home", action = "Index" } // Default values provided for the route URL
+            );
+
+        }
+    }
+}
---
+++ Assignment-04/EasyBootstrapMvc/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System.Web.Mvc;
+using System.Linq;
+
+namespace EasyBootstrapMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.PageName = "Index";
+            return View();
+        }
+
+        // GET: Home/MVC
+        public ActionResult MVC()
+        {
+            ViewBag.PageName = "MVC";
+            ViewData["DropDownData"] = new string[] { "cat", "hat" }.Select(x => new SelectListItem { Text = x, Value = x });
+            return View();
+        }
+
+        // GET: Home/JQuery
+        public ActionResult JQuery()
+        {
+            ViewBag.PageName = "JQuery";
+            return View();
+        }
+
+        // GET: Home/Bootstrap
+        [Route]
+        public ActionResult Bootstrap()
+        {
+            ViewBag.PageName = "Bootstrap";
+            return View();
+        }
+
+        //
+        // TODO: Code for a new route (e.g. "Cats") should go here
+        //
+        public ActionResult Cats()
+        {
+            ViewBag.PageName = "Cats";
+            return View();
+        }
+    }
+}
